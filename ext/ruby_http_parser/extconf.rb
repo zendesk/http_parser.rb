7d3dc76768c42b28ac23c5423960d555a5e6a6fe
Aman Gupta <aman@tmm1.net>
2010-10-19 23:38:20 -0700

    Move sources into vendor so they are easier to compile against

--- ext/ruby_http_parser/extconf.rb
+++ ext/ruby_http_parser/extconf.rb
@@ -1,6 +1,6 @@
 require 'mkmf'
 
-http_parser_dir = File.expand_path(File.dirname(__FILE__) + '/../http-parser')
+http_parser_dir = File.expand_path('../vendor/http-parser', __FILE__)
 $CFLAGS << " -I#{http_parser_dir} "
 
 dir_config("ruby_http_parser")
